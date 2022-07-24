d0bfe8275a468a46cc700eaeddc95938f1d95b75
Jack Yeung <jyeung.se@gmail.com>
2022-07-24 13:30:13 -0400

    test implementation of table with 1 stock.

--- src/Stockdata.tsx
+++ src/Stockdata.tsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
 import { Table } from 'antd';
-import type { ColumnsType, TableProps } from 'antd/lib/table';
+import type { ColumnsType } from 'antd/lib/table';
 
 export default function StockData() {
 
@@ -74,81 +74,85 @@ export default function StockData() {
 
 
 
-    // interface DataType {
-    //     key: React.Key;
-    //     name: string;
-    //     age: number;
-    //     address: string;
-    //   }
+    interface TableDataType {
+        key: React.Key;
+        name: string;
+        age: number;
+        address: string;
+      }
 
 
-    // const columns: ColumnsType<DataType> = [
-    // {
-    //     title: 'Name',
-    //     dataIndex: 'name',
-    //     filters: [
-    //     {
-    //         text: 'Joe',
-    //         value: 'Joe',
-    //     },
-    //     {
-    //         text: 'Jim',
-    //         value: 'Jim',
-    //     },
-    //     {
-    //         text: 'Submenu',
-    //         value: 'Submenu',
-    //         children: [
-    //         {
-    //             text: 'Green',
-    //             value: 'Green',
-    //         },
-    //         {
-    //             text: 'Black',
-    //             value: 'Black',
-    //         },
-    //         ],
-    //     },
-    //     ],
-    //     // specify the condition of filtering result
-    //     // here is that finding the name started with `value`
-    //     onFilter: (value: string, record) => record.name.indexOf(value) === 0,
-    //     sorter: (a, b) => a.name.length - b.name.length,
-    //     sortDirections: ['descend'],
-    // },
-    // {
-    //     title: 'Age',
-    //     dataIndex: 'age',
-    //     defaultSortOrder: 'descend',
-    //     sorter: (a, b) => a.age - b.age,
-    // },
-    // {
-    //     title: 'Address',
-    //     dataIndex: 'address',
-    //     filters: [
-    //     {
-    //         text: 'London',
-    //         value: 'London',
-    //     },
-    //     {
-    //         text: 'New York',
-    //         value: 'New York',
-    //     },
-    //     ],
-    //     onFilter: (value: string, record) => record.address.indexOf(value) === 0,
-    // },
-    // ];
+    const columns: ColumnsType<TableDataType> = [
+    {
+        title: 'Name',
+        dataIndex: 'name',
+        key: 'name',
+        render: text => <a>{text}</a>
+    },
+    {
+        title: 'Symbol',
+        dataIndex: 'symbol',
+        key: 'symbol'
+    },
+    {
+        title: 'Price',
+        dataIndex: 'price',
+        key: 'price'
+    },
+    {
+        title: 'EPS',
+        dataIndex: 'eps',
+        key: 'eps'
+    },
+    {
+        title: 'DayHigh',
+        dataIndex: 'dayHigh',
+        key: 'dayHigh'
+    },
+    {
+        title: 'DayLow',
+        dataIndex: 'dayLow',
+        key: 'dayLow'
+    },
+    {
+        title: 'Change',
+        dataIndex: 'change',
+        key: 'change'
+    }
+    ];
 
 
-    // let data = 
+    const data = [
+        {
+          key: '1',
+          name: 'John Brown',
+          age: 32,
+          address: 'New York No. 1 Lake Park',
+          tags: ['nice', 'developer'],
+        },
+        {
+          key: '2',
+          name: 'Jim Green',
+          age: 42,
+          address: 'London No. 1 Lake Park',
+          tags: ['loser'],
+        },
+        {
+          key: '3',
+          name: 'Joe Black',
+          age: 32,
+          address: 'Sidney No. 1 Lake Park',
+          tags: ['cool', 'teacher'],
+        },
+      ];
 
-
+      
     // const onChange: TableProps<DataType>['onChange'] = (pagination, filters, sorter, extra) => {
     // console.log('params', pagination, filters, sorter, extra);
     // };
       
-    // const App: React.FC = () => <Table columns={columns} dataSource={data} onChange={onChange} />;
-
+    const stockTable = () => <Table columns={columns} dataSource={data} />;
+        
 
 
 
@@ -159,6 +163,7 @@ export default function StockData() {
 
     return (
        <div>
+        {stockTable()}
         {stockOverviewDisplayed()}
         {stockHourlyDisplayed()}
         </div>
