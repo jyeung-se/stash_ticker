a22053ff418d98022cafd8c8553befcbd9331aaf
Jack Yeung <jyeung.se@gmail.com>
2022-08-01 23:20:52 -0400

    Imported new react router v6. Need to create the routes still.

--- src/Stockdata.tsx
+++ src/Stockdata.tsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import axios from "axios";
 import './App.css';
 import { Table } from 'antd';
