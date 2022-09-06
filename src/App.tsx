92723e3d9adbcb8a62bb8e26546879b244cc5772
Jack Yeung <jyeung.se@gmail.com>
2022-09-06 18:32:07 -0400

    First deploy to Netlify. Swapped API key for usage.

--- src/App.tsx
+++ src/App.tsx
@@ -28,12 +28,12 @@ const App = () => {
   const [stockResults, setStockResults] = useState<any>([])
   const [stockHourlyResults, setStockHourlyResults] = useState<any>([])
   const [mostRecentSearch, setMostRecentSearch] = useState('')
-  const [stockStash, setStockStash] = useState<any>([])
+//   const [stockStash, setStockStash] = useState<any>([])
   const [allStocksTableVisability, setAllStocksTableVisability] = useState(true)
   const [timePeriod, setTimePeriod] = useState('1D')
   const [stockTimePeriodResults, setStockTimePeriodResults] = useState<any>([])
   const [companyProfile, setCompanyProfile] = useState<any>([])
-  const [filteredStockTickers, setFilteredStockTickers] = useState<any>([])
+//   const [filteredStockTickers, setFilteredStockTickers] = useState<any>([])
   const [isReadMore, setIsReadMore] = useState(true);
   const [stockPriceDollarChange, setStockPriceDollarChange] = useState(0)
   const [stockPricePercentChange, setStockPricePercentChange] = useState(0)
@@ -95,7 +95,7 @@ const App = () => {
     useEffect(() => {   
         // API Calls for ALL stocks
         //Endpoint = Symbols List
-        fetch('https://financialmodelingprep.com/api/v3/stock/list?apikey=4672ed38f1e727b95f8a9cbd22574eed').then(async (res) => {
+        fetch('https://financialmodelingprep.com/api/v3/stock/list?apikey=f2fd9f5601de912d73c808de0f575e3f').then(async (res) => {
             const stockData = await res.json()
             // console.log('stockData[0]:', stockData[0]);
             setAllStocks(stockData)
@@ -128,7 +128,7 @@ const App = () => {
         })
 
         if (lastSearch !== '') {
-            fetch(`https://financialmodelingprep.com/api/v3/historical-price-full/${lastSearch}?timeseries=${targetDays}&apikey=4672ed38f1e727b95f8a9cbd22574eed`)
+            fetch(`https://financialmodelingprep.com/api/v3/historical-price-full/${lastSearch}?timeseries=${targetDays}&apikey=f2fd9f5601de912d73c808de0f575e3f`)
             .then((res) => {
                 return res.json()
             })
@@ -139,11 +139,11 @@ const App = () => {
     }, [lastSearch])
 
       
-    const snapshotTable = () => <Table className="flex-container" columns={snapshotColumns} dataSource={stockResults} />;
-    const myStashTable = () => <Table className="flex-container" columns={myStashColumns} dataSource={stockStash} />;
-    const allStocksTable = () => <Table className="flex-container" columns={allStocksColumns} dataSource={allStocks} />;
-    const hourlyStockTable = () => <Table className="flex-container" columns={timePeriodColumns} dataSource={stockHourlyResults} />; 
-    const timePeriodStockTable = () => <Table className="flex-container" columns={timePeriodColumns} dataSource={stockTimePeriodResults} />; 
+    // const snapshotTable = () => <Table className="flex-container" columns={snapshotColumns} dataSource={stockResults} />;
+    // const myStashTable = () => <Table className="flex-container" columns={myStashColumns} dataSource={stockStash} />;
+    // const hourlyStockTable = () => <Table className="flex-container" columns={timePeriodColumns} dataSource={stockHourlyResults} />; 
+    // const timePeriodStockTable = () => <Table className="flex-container" columns={timePeriodColumns} dataSource={stockTimePeriodResults} />; 
+    const allStocksTable = () => <Table className="flex-container" columns={allStocksColumns} dataSource={allStocks} />
 
 
     const hourlyStockChart = () => {
@@ -221,74 +221,75 @@ const App = () => {
     }
 
 
-    const compareForSorting = (a: any, b: any) => {
-        // console.log("compareForSorting fires off")
-        if ( a.symbol < b.symbol ){
-            return -1;
-        }
-        if ( a.symbol > b.symbol ){
-            return 1;
-        }
-        return 0;
-    }
+    // const compareForSorting = (a: any, b: any) => {
+    //     // console.log("compareForSorting fires off")
+    //     if ( a.symbol < b.symbol ){
+    //         return -1;
+    //     }
+    //     if ( a.symbol > b.symbol ){
+    //         return 1;
+    //     }
+    //     return 0;
+    // }
 
 
-    const addToStockStash = () => {
-        // console.log('stockResults is: ', stockResults)
-        // console.log('stockStash before updating is: ', stockStash)
-        const listOfStockSymbolsInStash: any = () => {
-            return stockStash.map((stock: any) => {
-                return stock.symbol
-            })
-        }
-        if (listOfStockSymbolsInStash().includes(stockResults[0].symbol)) {
-            return alert('The stock has already been added to your Stash before. Please add a different stock instead.')
-        }   
-        setStockStash([...stockStash, ...stockResults].sort(compareForSorting))
-        // console.log('stockStash AFTER updating is: ', stockStash)
-    }
+    // const addToStockStash = () => {
+    //     // console.log('stockResults is: ', stockResults)
+    //     // console.log('stockStash before updating is: ', stockStash)
+    //     const listOfStockSymbolsInStash: any = () => {
+    //         return stockStash.map((stock: any) => {
+    //             return stock.symbol
+    //         })
+    //     }
+    //     if (listOfStockSymbolsInStash().includes(stockResults[0].symbol)) {
+    //         return alert('The stock has already been added to your Stash before. Please add a different stock instead.')
+    //     }   
+    //     setStockStash([...stockStash, ...stockResults].sort(compareForSorting))
+    //     // console.log('stockStash AFTER updating is: ', stockStash)
+    // }
 
 
-    const tablesShownPostSearch = () => {
-        // console.log(mostRecentSearch, stockResults, stockHourlyResults)
-        if (stockResults.length > 0) {
-            return (
-                <div>
-                    <br></br>
-                    {<h2>My Stock Stash</h2>}
-                    {myStashTable()}
-                    <br></br>
-                    <h2>{stockResults[0].symbol} Profile</h2>
-                    {snapshotTable()}
-                    <button type="button" className="addToStockStashButton" onClick={addToStockStash}>Add to Stash</button>
-                    <br></br>
-                    <br></br>
-                    {hourlyStockChart()}
-                    <br></br>
-                    <h2>{stockResults[0].symbol} Hourly Historicals</h2>
-                    {hourlyStockTable()}
-                    <br></br>
-                </div>
-            )
-        }
-    }   
+    // const tablesShownPostSearch = () => {
+    //     // console.log(mostRecentSearch, stockResults, stockHourlyResults)
+    //     if (stockResults.length > 0) {
+    //         return (
+    //             <div>
+    //                 <br></br>
+    //                 {<h2>My Stock Stash</h2>}
+    //                 {myStashTable()}
+    //                 <br></br>
+    //                 <h2>{stockResults[0].symbol} Profile</h2>
+    //                 {snapshotTable()}
+    //                 <button type="button" className="addToStockStashButton" onClick={addToStockStash}>Add to Stash</button>
+    //                 <br></br>
+    //                 <br></br>
+    //                 {hourlyStockChart()}
+    //                 <br></br>
+    //                 <h2>{stockResults[0].symbol} Hourly Historicals</h2>
+    //                 {hourlyStockTable()}
+    //                 <br></br>
+    //             </div>
+    //         )
+    //     }
+    // }   
 
 
-    const toggleAllStocksTable = () => {
-      setAllStocksTableVisability(!allStocksTableVisability)
-      // console.log('stocktablevisibility state after toggle: ', AllStocksTableVisability)
-    }
+    // const toggleAllStocksTable = () => {
+    //   setAllStocksTableVisability(!allStocksTableVisability)
+    //   // console.log('stocktablevisibility state after toggle: ', AllStocksTableVisability)
+    // }
+
 
     const displayAllStocksTable = () => {
-        return allStocksTableVisability === true ? <div><h2>All Companies</h2> <br></br> {allStocksTable()}</div> : null
+        return allStocksTableVisability === true ? <div><br /><h2>All Companies</h2> <br></br> {allStocksTable()}</div> : null
     }
 
 
     const fetchStockInfo = async () => {
         const [stockData, stockHourlyData, companyProfileData] = await Promise.all([
-            fetch(`https://financialmodelingprep.com/api/v3/quote/${mostRecentSearch}?apikey=4672ed38f1e727b95f8a9cbd22574eed`),
-            fetch(`https://financialmodelingprep.com/api/v3/historical-chart/1hour/${mostRecentSearch}?apikey=4672ed38f1e727b95f8a9cbd22574eed`),
-            fetch(`https://financialmodelingprep.com/api/v3/profile/${mostRecentSearch}?apikey=4672ed38f1e727b95f8a9cbd22574eed`)
+            fetch(`https://financialmodelingprep.com/api/v3/quote/${mostRecentSearch}?apikey=f2fd9f5601de912d73c808de0f575e3f`),
+            fetch(`https://financialmodelingprep.com/api/v3/historical-chart/1hour/${mostRecentSearch}?apikey=f2fd9f5601de912d73c808de0f575e3f`),
+            fetch(`https://financialmodelingprep.com/api/v3/profile/${mostRecentSearch}?apikey=f2fd9f5601de912d73c808de0f575e3f`)
         ])
 
         const stocks = await stockData.json()
@@ -326,7 +327,7 @@ const App = () => {
             }
         })
 
-        fetch(`https://financialmodelingprep.com/api/v3/historical-price-full/${mostRecentSearch}?timeseries=${targetDays}&apikey=4672ed38f1e727b95f8a9cbd22574eed`)
+        fetch(`https://financialmodelingprep.com/api/v3/historical-price-full/${mostRecentSearch}?timeseries=${targetDays}&apikey=f2fd9f5601de912d73c808de0f575e3f`)
         .then((res) => {
             return res.json()
         })
@@ -369,7 +370,6 @@ const App = () => {
             setAllStocksTableVisability(false)
             displayAllStocksTable()
             setIsReadMore(true)
-            setMostRecentSearch('')
         } else {
             alert("Please check to see if you have entered a correct stock symbol, then try again.")
         }
@@ -384,7 +384,7 @@ const App = () => {
             // console.log(mostRecentSearch)
 
             setTimeout(async () => {
-                await fetch(`https://financialmodelingprep.com/api/v3/search-ticker?query=${value}&limit=10&apikey=4672ed38f1e727b95f8a9cbd22574eed`)
+                await fetch(`https://financialmodelingprep.com/api/v3/search-ticker?query=${value}&limit=10&apikey=f2fd9f5601de912d73c808de0f575e3f`)
                 .then((res) => {
                     return res.json()
                 })
@@ -396,6 +396,7 @@ const App = () => {
               }, 1500)
         }
 
+        let inputRef
 
         return (
             <form onSubmit={(e) => handleSubmit(e)} className="async-search-field">
@@ -407,10 +408,10 @@ const App = () => {
                     style={{ width: 300 }}
                     open={open}
                     onOpen={() => {
-                        setOpen(true);
+                        setOpen(true)
                     }}
                     onClose={() => {
-                        setOpen(false);
+                        setOpen(false)
                     }}
                     isOptionEqualToValue={(option: any, value: any) => option.symbol === value.symbol}
                     getOptionLabel={option => option.symbol}
@@ -424,8 +425,12 @@ const App = () => {
                         onChange={e => {
                             // dont fire API if the input is blank or empty
                             if (e.target.value !== "" || e.target.value !== null || mostRecentSearch !== "" || mostRecentSearch !== null) {
-                            onChangeHandle(e.target.value)
+                                onChangeHandle(e.target.value)
                             }
+                        }}
+                        inputRef={input => {
+                            inputRef = input
+                            // console.log('inputRef is: ', inputRef)
                         }}
                         InputProps={{
                             ...params.InputProps,
@@ -497,22 +502,17 @@ const App = () => {
             <div>
                 <div className="stock-header">
                     <Divider orientation="left"></Divider>
-                    {(stockResults.length || stockHourlyResults.length !== 0) ? <h1 className="stock-name">{stockResults[0].name} ({stockResults[0].symbol})</h1> : null} 
+                    {(stockResults.length !== 0 || stockHourlyResults.length !== 0) ? <h1 className="stock-name">{stockResults[0].name} ({stockResults[0].symbol})</h1> : null} 
                     <br />
                     <h1 className="stock-price">${stockResults[0].price}</h1> 
                     <br />
-
                     {chartHeaderPriceStats()}
-                    {/* <h1 className="stock-price">${stockResults[0].price}</h1> 
-                    <br />
-                    {stockResults[0].changesPercentage > 0 ? <h2 className="stock-change-up">$&nbsp;{(Math.round((stockResults[0].change + Number.EPSILON).toFixed * 100) / 100).toFixed(2)} ({Math.round((stockResults[0].changesPercentage + Number.EPSILON) * 100) / 100}%) Today</h2> : <h2 className="stock-change-down">${(Math.round((stockResults[0].change + Number.EPSILON) * 100) / 100).toFixed(2)} ({Math.round((stockResults[0].changesPercentage + Number.EPSILON) * 100) / 100}%) Today</h2>} */}
-
                 </div>
-                    {showSelectedPeriodChart()}
-                    <br />
-                    <ul className="chart-buttons">
-                        {chartButtons()}
-                    </ul>
+                {showSelectedPeriodChart()}
+                <br />
+                <ul className="chart-buttons">
+                    {chartButtons()}
+                </ul>
                 <br />
                 <br />
                 <Row>
@@ -529,7 +529,7 @@ const App = () => {
                     </Col>
                     <Col span={3}>
                         <h3 className="h3-left">Price-Earnings ratio</h3>
-                        <h3 className="h3-about-data">{stockResults[0].pe}</h3>
+                        <h3 className="h3-about-data">{stockResults[0].pe.toFixed(2)}</h3>
                         <h3 className="h3-left">Low today</h3>
                         <h3 className="h3-about-data">${stockResults[0].dayLow.toFixed(2)}</h3>
                         <h3 className="h3-left">52 Week low</h3>
